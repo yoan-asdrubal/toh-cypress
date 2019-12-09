d0d5b2e006a3a632af8e7db3d0b2252ffd273f6f
Yoan Asdrubal <yoan.asdrubal.kubano@gmail.com>
2019-12-09 16:54:14 -0500

    Add(Dashboard):Agrega modulo,componente y opcion para navegacion de dashboard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [{
+    path: 'dashboard',
+    loadChildren: () => import('./cy-app/dasboard/dasboard.module').then((m) => m.DasboardModule)
+}];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
