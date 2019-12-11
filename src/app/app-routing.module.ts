32b6ca28c2694b205103d78470a370e020b3d88c
Yoan Asdrubal <yoan.asdrubal.kubano@gmail.com>
2019-12-10 23:42:11 -0500

    Refactor(App):Agrega configuracion de rutas para path not found

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
+import {Error404Component} from './layout/components/error404/error404.component';
 
-const routes: Routes = [{
-    path: 'dashboard',
-    loadChildren: () => import('./cy-app/dasboard/dasboard.module').then((m) => m.DasboardModule)
-}];
+const routes: Routes = [
+    {
+        path: 'dashboard',
+        loadChildren: () => import('./cy-app/dasboard/dasboard.module').then((m) => m.DasboardModule)
+    },
+    {
+        path: 'pathNotFound', component: Error404Component
+    },
+    {
+        path: '**', redirectTo: 'pathNotFound'
+    }
+
+];
 
 @NgModule({
     imports: [RouterModule.forRoot(routes)],
