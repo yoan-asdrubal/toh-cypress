f99f1e06c261fea37c5aa5de7d66ed51f59c5586
Yoan Asdrubal <yoan.asdrubal.kubano@gmail.com>
2019-12-11 15:40:30 -0500

    Refactor(HeroesModule):Agrega modulo de material para manejo de botones

--- src/app/cy-app/heroes/heroes.module.ts
+++ src/app/cy-app/heroes/heroes.module.ts
@@ -5,14 +5,16 @@ import {HeroesRoutingModule} from './heroes-routing.module';
 import {HeroesListComponent} from './component/heroes-list/heroes-list.component';
 import {HeroesFormComponent} from './component/heroes-form/heroes-form.component';
 import {RxReactiveFormsModule} from '@rxweb/reactive-form-validators';
+import {MatButtonModule} from '@angular/material';
 
 @NgModule({
     declarations: [HeroesListComponent, HeroesFormComponent],
-    imports: [
-        CommonModule,
-        RxReactiveFormsModule,
-        HeroesRoutingModule
-    ]
+  imports: [
+    CommonModule,
+    RxReactiveFormsModule,
+    HeroesRoutingModule,
+    MatButtonModule
+  ]
 })
 export class HeroesModule {
 }
