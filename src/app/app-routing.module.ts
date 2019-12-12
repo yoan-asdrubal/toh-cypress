2ef0c53e032890c637c021f3850f8dace9994ff6
Yoan Asdrubal <yoan.asdrubal.kubano@gmail.com>
2019-12-12 15:06:08 -0500

    Refactor(App):Modifica routing para agregar router outlet al app component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,26 +1,31 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {Error404Component} from './layout/components/error404/error404.component';
-import {TohHomeComponent} from './layout/components/toh-home/toh-home.component';
+import {NavigationComponent} from '@app/layout/components/navigation/navigation.component';
+import {TohHomeComponent} from '@app/layout/components/toh-home/toh-home.component';
 
 const routes: Routes = [
     {
-        path: 'dashboard',
-        loadChildren: () => import('./cy-app/dasboard/dasboard.module').then((m) => m.DasboardModule)
+        path: '',
+        component: NavigationComponent,
+        children: [
+            {
+                path: 'dashboard',
+                loadChildren: () => import('./cy-app/dasboard/dasboard.module').then((m) => m.DasboardModule)
+            },
+            {
+                path: 'heroes',
+                loadChildren: () => import('./cy-app/heroes/heroes.module').then((m) => m.HeroesModule)
+            },
+            {
+                path: 'pathNotFound', component: Error404Component
+            },
+            {path: '', component: TohHomeComponent},
+            {
+                path: '**', redirectTo: 'pathNotFound'
+            }]
     },
-    {
-        path: 'heroes',
-        loadChildren: () => import('./cy-app/heroes/heroes.module').then((m) => m.HeroesModule)
-    },
-    {
-        path: 'pathNotFound', component: Error404Component
-    },
-    {
-        path: '', pathMatch: 'full', component: TohHomeComponent
-    },
-    {
-        path: '**', redirectTo: 'pathNotFound'
-    }
+
 
 ];
 
